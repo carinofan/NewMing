389ba5739bf86bcb83a6e8b023ebc98015abace0
Fanming <fanming@FanM-iMac.local>
2015-07-22 15:47:36 +0800

    Initial Commit

---
+++ NewMing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewMing
+//
+//  Created by Fanming on 15/7/22.
+//  Copyright (c) 2015年 FanMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewMing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewMing
+//
+//  Created by Fanming on 15/7/22.
+//  Copyright (c) 2015年 FanMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
